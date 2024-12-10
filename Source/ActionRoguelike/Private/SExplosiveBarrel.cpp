88193c717de0eb8b97b553dc769d35767f25d686
Emmanuel Lauzon <xlauzon@gmail.com>
2024-12-09 20:07:01 -0500

    Create C++ Explosive Barrel (Almost Works)

---
+++ Source/ActionRoguelike/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComp"));
+	StaticMeshComp->SetCollisionProfileName(TEXT("PhysicsActor"));
+	StaticMeshComp->SetSimulatePhysics(true);
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComponent"));
+	RadialForceComp->Radius = 700;
+	RadialForceComp->ImpulseStrength = 2000.0f;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->SetupAttachment(StaticMeshComp);
+	
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ASExplosiveBarrel::OnComponentHit()
+{
+	// Fire the Radial Impulse
+	RadialForceComp->FireImpulse();
+}
+
